2d7c40b2399b956b85dee3bb178817799dd239dd
helyao <helyao@qq.com>
2017-09-14 17:11:50 +0800

    add baidu statistics

---
+++ js/baidu_statistics.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?7892bc7fef787d7f0355792674568608";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
